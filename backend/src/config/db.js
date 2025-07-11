7ca091145abf6d071ec803501b3327cb9a215bd3
Krishna Sai Jekkuleti <123238540+master8889@users.noreply.github.com>
2025-07-11 12:39:42 +0530

    Delete backend directory

--- backend/src/config/db.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true });
-module.exports = mongoose.connection;
--- backend/src/middleware/logging.js
+++
@@ -1,5 +0,0 @@
-const loggingMiddleware = (req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-};
-module.exports = loggingMiddleware;
